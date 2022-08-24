b885a0f84149e785f0f6bc566722bd04eb3b452f
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-08-24 11:26:46 -0400

    Added argparser to install.py Removed namespacing for now

--- scripts/ros_config.sh
+++ scripts/ros_config.sh
@@ -27,12 +27,12 @@ done
 
 read -p "ROS_DOMAIN_ID (default 0): " ros_domain_id
 ros_domain_id=${ros_domain_id:-0}
-read -p "ROS namespace (default empty): " namespace
+#read -p "ROS namespace (default empty): " namespace
 read -p "RMW Implementation [rmw_cyclonedds_cpp,rmw_fastrtps_cpp] (default rmw_cyclonedds_cpp): " rmw
 rmw=${rmw:-rmw_cyclonedds_cpp}
 
 echo "ROS_DOMAIN_ID: $ros_domain_id";
-echo "Namespace: $namespace";
+#echo "Namespace: $namespace";
 echo "RMW_IMPLEMENTATION: $rmw";
 read -p "Press enter to apply these settings."
 
@@ -41,12 +41,12 @@ ttb4=$( cat /etc/turtlebot4 )
 model="standard"
 
 case $ttb4 in
-    *"Lite"*)
+    *"lite"*)
     model="lite";;
 esac
 
 # Set Create 3 configuration
-curl -d "ros_domain_id=$ros_domain_id&ros_namespace=$namespace&rmw_implementation=$rmw" -X POST http://192.168.186.2/ros-config-save-main -o /dev/null
+curl -d "ros_domain_id=$ros_domain_id&ros_namespace=&rmw_implementation=$rmw" -X POST http://192.168.186.2/ros-config-save-main -o /dev/null
 
 # Reboot Create 3
 curl -X POST http://192.168.186.2/api/reboot
@@ -58,7 +58,7 @@ sudo systemctl stop turtlebot4.service
 uninstall.py
 
 # Install robot_upstart job with new ROS_DOMAIN_ID
-install.py $model $ros_domain_id
+install.py $model --domain $ros_domain_id --rmw $rmw
 
 # Start job
 sudo systemctl daemon-reload && sudo systemctl start turtlebot4
@@ -78,4 +78,4 @@ else
     echo "export ROS_DOMAIN_ID=$ros_domain_id" | sudo tee -a ~/.bashrc
 fi
 
-source ~/.bashrc
+echo "Source ~/.bashrc to apply these changes to this terminal."
