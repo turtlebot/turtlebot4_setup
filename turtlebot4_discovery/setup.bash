1f9348921f1ec89c42a6f15aeafb16c9dc9de12f
Hilary Luo <hluo@clearpathrobotics.com>
2024-03-14 13:22:35 -0400

    Switch from xml super client to envar

--- turtlebot4_discovery/setup.bash
+++ turtlebot4_discovery/setup.bash
@@ -1,5 +1,6 @@
 source /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=/etc/turtlebot4_discovery/fastdds_discovery_super_client.xml
+export FASTRTPS_DEFAULT_PROFILES_FILE=/etc/turtlebot4/fastdds_rpi.xml
 export ROS_DISCOVERY_SERVER=10.42.0.1:11811
 export ROS_DOMAIN_ID=0
+export ROS_SUPER_CLIENT=True
