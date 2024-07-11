05f7f7573013c4d5fa04a8f326a0a85a25bef712
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2024-07-11 13:03:14 -0400

    Add docker installation to jazzy.sh

--- scripts/jazzy.sh
+++ scripts/jazzy.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install curl gnupg lsb-release -y
+
+# Add ROS sources
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Add Docker sources
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install the packages
 sudo apt update
 sudo apt install -y \
 ros-jazzy-ros-base \
@@ -12,4 +25,12 @@ wget \
 ros-dev-tools \
 socat \
 network-manager \
-chrony
+chrony \
+docker-ce \
+docker-ce-cli \
+containerd.io \
+docker-buildx-plugin \
+docker-compose-plugin
+
+# Docker post-install configuration
+sudo usermod -a -G docker $(whoami)
